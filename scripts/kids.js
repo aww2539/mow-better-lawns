73f68f2f83968942cbff97eb231ffb2e5a9f0687
Alex Woodall <aww2539@gmail.com>
2021-07-27 14:40:34 -0500

    imported kids to html

---
+++ scripts/kids.js
@@ -0,0 +1,18 @@
+// module to hold information related to kids and render this information to the DOM
+
+import { getHomes, getKidHomeAssignments, getKids, getServices } from "./database.js";
+
+const kids = getKids()
+const homes = getHomes()
+const services = getServices()
+const kidHomeAssignments = getKidHomeAssignments()
+
+export const Kids = () => {
+    let html = ""
+    html = "<ul>"
+    for (const kid of kids) {
+        html += `<li id=kid--${kid.id}>${kid.name}</li>`
+    }
+    html += "</ul"
+    return html
+}
